33c4f95b3b397b9ecc55de4564a8371dee2191a7
as718296 <alex718296@outlook.com>
2023-02-26 01:14:12 +0800

    w02

---
+++ demo/W01/src/data.sql
@@ -0,0 +1,6 @@
+INSERT　INTO cart-69 (id,title,price,img,amount)
+VALUES
+(5,Samsung Galaxy A53,599.99,https://ufpyfzqptcdbelaugszo.supabase.co/storage/v1/object/public/demo-69/cart-69/galaxy-S8.png,1),
+(6,google pixel 7,499.99,https://ufpyfzqptcdbelaugszo.supabase.co/storage/v1/object/public/demo-69/cart-69/google-pixel.png,1),
+(7,Xiaomi 13,699.99,https://ufpyfzqptcdbelaugszo.supabase.co/storage/v1/object/public/demo-69/cart-69/note-2.png,1),
+(8,Samsung Galaxy S1,399.99 ,https://ufpyfzqptcdbelaugszo.supabase.co/storage/v1/object/public/demo-69/cart-69/galaxy-S7.png,1)
