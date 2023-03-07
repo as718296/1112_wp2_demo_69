1a128ad07ceccd4472b527f490a1cd879020820e
as718296 <alex718296@outlook.com>
2023-03-07 20:35:02 +0800

    W03-P3: create card_xx tale in SUPABASE

--- utils/database.js
+++ utils/database.js
@@ -1,15 +1,29 @@
 const { Pool } = require('pg');
 
-const pool = new Pool({
-  user: 'postgres',
-  host: 'db.ufpyfzqptcdbelaugszo.supabase.co',
-  port: '5432',
-  database: 'postgres',
-  password: '1qaz@WSX56615829',
-});
+let pool;
 
-console.log(
-  `Connecting  local PostgreSQL database ${pool.options.database} on port ${pool.options.port}`
-);
+if (process.env.NODE_ENV === 'SUPABASE') {
+  pool = new Pool({
+    user: 'postgres',
+    host: process.env.SUPABASE_HOST,
+    port: '5432',
+    database: 'postgres',
+    password: process.env.SUPABASE_PASSWORD,
+  });
+  console.log(
+    `Connecting  local SUPABASEF PostgreSQL database ${pool.options.database} on port ${pool.options.port}`
+  );
+} else {
+  pool = new Pool({
+    user: 'postgres',
+    host: 'localhost',
+    port: '5432',
+    database: 'postgres',
+    password: '1qaz@WSX56615829',
+  });
+  console.log(
+    `Connecting  local PostgreSQL database ${pool.options.database} on port ${pool.options.port}`
+  );
+}
 
 module.exports = pool;
